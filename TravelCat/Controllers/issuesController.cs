c2582a6933552896b5d84d4c96b703cd1809f52c
prochini <42841117+prochini@users.noreply.github.com>
2019-10-31 16:41:09 +0800

    issue/member

---
+++ TravelCat/Controllers/issuesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TravelCat.Models;
+
+namespace TravelCat.Controllers
+{
+    public class issuesController : Controller
+    {
+        private dbTravelCat db = new dbTravelCat();
+
+        // GET: issues
+        public ActionResult Index()
+        {
+
+            var issues = db.issues.Include(i => i.admin).Include(i => i.issue_type).Include(i => i.member);
+            return View(issues.ToList());
+        }
+
+        // GET: issues/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            issue issue = new issue();
+            issue = db.issues.Where(m=>m.id==id).FirstOrDefault();
+
+            //issue issue. = db.issues.Find(id);
+            if (issue == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.admin_id = new SelectList(db.admins, "admin_id", "admin_account", issue.admin_id);
+            ViewBag.issue_id = new SelectList(db.issue_type, "issue_id", "issue_name", issue.issue_id);
+            ViewBag.member_id = new SelectList(db.members, "member_id", "member_account", issue.member_id);
+            return View(issue);
+        }
+
+        // POST: issues/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,member_id,admin_id,issue_id,report_date,issue_content,issue_result,issue_status,resolve_date")] issue issue)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(issue).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(issue);
+        }
+
+
+        
+    }
+}
--- TravelCat/Controllers/membersController.cs
+++ TravelCat/Controllers/membersController.cs
@@ -10,55 +10,19 @@ using TravelCat.Models;
 
 namespace TravelCat.Controllers
 {
-    public class membersController : Controller
+    public class members1Controller : Controller
     {
         private dbTravelCat db = new dbTravelCat();
 
-        // GET: members
+        // GET: members1
         public ActionResult Index()
         {
             return View(db.members.ToList());
         }
 
-        // GET: members/Details/5
-        public ActionResult Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            member member = db.members.Find(id);
-            if (member == null)
-            {
-                return HttpNotFound();
-            }
-            return View(member);
-        }
+        
 
-        // GET: members/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: members/Create
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "member_id,member_account,member_password,member_status")] member member)
-        {
-            if (ModelState.IsValid)
-            {
-                db.members.Add(member);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(member);
-        }
-
-        // GET: members/Edit/5
+        // GET: members1/Edit/5
         public ActionResult Edit(string id)
         {
             if (id == null)
@@ -73,13 +37,14 @@ namespace TravelCat.Controllers
             return View(member);
         }
 
-        // POST: members/Edit/5
+        // POST: members1/Edit/5
         // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
         // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "member_id,member_account,member_status")] member member)
+        public ActionResult Edit([Bind(Include = "member_id,member_account,member_password,member_status")] member member)
         {
+            
             if (ModelState.IsValid)
             {
                 db.Entry(member).State = EntityState.Modified;
@@ -89,39 +54,6 @@ namespace TravelCat.Controllers
             return View(member);
         }
 
-        // GET: members/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            member member = db.members.Find(id);
-            if (member == null)
-            {
-                return HttpNotFound();
-            }
-            return View(member);
-        }
-
-        // POST: members/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            member member = db.members.Find(id);
-            db.members.Remove(member);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        
     }
 }
