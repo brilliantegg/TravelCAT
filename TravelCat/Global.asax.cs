c129d5a4bfbc6c0b477e20a1930d7e59c42d8610
prochini <42841117+prochini@users.noreply.github.com>
2019-11-06 10:02:05 +0800

    origin global

--- TravelCat/Global.asax.cs
+++ TravelCat/Global.asax.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
-using System.Web.Http;
+
 using System.Data.Entity;
 
 
@@ -15,33 +15,18 @@ using System.Data.Entity;
 
 namespace TravelCat
 {
-    //public class Global : System.Web.HttpApplication
-    //{
-    //    protected void Application_Start(object sender, EventArgs e)
-    //    {
-    //        AreaRegistration.RegisterAllAreas();
-    //        WebApiConfig.Register(System.Web.Http.GlobalConfiguration.Configuration);
-    //        RouteConfig.RegisterRoutes(RouteTable.Routes);
-    //        GlobalConfiguration.Configure(WebApiConfig.Register);
-    //    }
-    //}
+
     public class MvcApplication : System.Web.HttpApplication
     {
 
         protected void Application_Start(object sender, EventArgs e)
         {
             AreaRegistration.RegisterAllAreas();
-            WebApiConfig.Register(System.Web.Http.GlobalConfiguration.Configuration);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            //System.Web.Http.GlobalConfiguration.Configure(WebApiConfig.Register);
-
-            System.Web.Http.GlobalConfiguration.Configuration.EnsureInitialized();
+            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);         
 
 
-            System.Web.Http.GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
-            .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-            System.Web.Http.GlobalConfiguration.Configuration.Formatters
-                .Remove(System.Web.Http.GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+            
 
         }
 
