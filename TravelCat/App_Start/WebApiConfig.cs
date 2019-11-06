6fb0e67e9c2377073469805ff545f139cc6b3849
prochini <42841117+prochini@users.noreply.github.com>
2019-11-06 08:55:43 +0800

    單一觀光頁面 viewmodel + webapi 設定 global.asax
    單一觀光頁面 viewmodel : 觀光+評論+留言
    
     webapi 設定 global.asax ,webapi config

---
+++ TravelCat/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace TravelCat
+{
+    public static class WebApiConfig
+    {
+
+
+        public static void Register(HttpConfiguration config)
+        {
+            System.Web.Http.GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
