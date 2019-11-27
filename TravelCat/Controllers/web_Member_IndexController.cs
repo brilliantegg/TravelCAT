ae8c19f54b0a39f90d203216308f9d95dcf1ac6e
a6518470 <57085191+a6518470@users.noreply.github.com>
2019-11-27 10:47:13 +0800

    更改密碼

--- TravelCat/Controllers/web_Member_IndexController.cs
+++ TravelCat/Controllers/web_Member_IndexController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -42,9 +43,51 @@ namespace TravelCat.Controllers
         }
 
 
-        public ActionResult Editpassword()
+        //修改密碼
+        public ActionResult Editpassword(string id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            member member = db.member.Find(id);
+
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editpassword(string id, string oldpassword, string newpassword)
+        {
+            member member = db.member.Find(id);
+            
+            byte[] password1 = System.Text.Encoding.UTF8.GetBytes(oldpassword);
+            byte[] hash1 = new System.Security.Cryptography.SHA256Managed().ComputeHash(password1);
+            string hashpassword1 = Convert.ToBase64String(hash1);
+
+            ViewBag.Err = "原密碼有誤";
+
+            if (hashpassword1 == member.member_password)
+            {
+                if (ModelState.IsValid)
+                {
+                    byte[] password = System.Text.Encoding.UTF8.GetBytes(newpassword);
+                    byte[] hash = new System.Security.Cryptography.SHA256Managed().ComputeHash(password);
+                    string hashpassword = Convert.ToBase64String(hash);
+
+                    member.member_password = hashpassword;
+
+                    db.Entry(member).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "web_Member_Index", new { id = member.member_id });
+                }
+            }
+            return View(member);
         }
     }
 }
