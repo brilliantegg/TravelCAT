da5b403a5b05054ccafca75ebc76d8ddc08622f9
brilliantegg <31965408+brilliantegg@users.noreply.github.com>
2019-12-13 13:15:33 +0800

    搜尋施工中(餐廳景點分頁壞掉)

--- TravelCat/ViewModels/SearchViewModel.cs
+++ TravelCat/ViewModels/SearchViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using TravelCat.Models;
+using X.PagedList.Mvc;
+using X.PagedList;
 
 namespace TravelCat.ViewModels
 {
@@ -14,8 +16,9 @@ namespace TravelCat.ViewModels
         public List<spot> spot { get; set; }
         public List<member_profile> member { get; set; }
         public List<comment> comment { get; set; }
+        public IPagedList<result_rating> show_ratings { get; set; }
         public List<result_rating> result_ratings { get; set; }
-     }
+    }
     public class result_rating 
     {
         public string id { get; set; }
