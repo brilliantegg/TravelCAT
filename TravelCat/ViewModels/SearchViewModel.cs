b8603b77107bf612b6ff9741c510a164e82c9a99
Audrey <audrey@appx.com.tw>
2020-10-05 01:09:03 +0800

    new viewmodel

--- TravelCat/ViewModels/SearchViewModel.cs
+++ TravelCat/ViewModels/SearchViewModel.cs
@@ -8,28 +8,39 @@ using X.PagedList;
 
 namespace TravelCat.ViewModels
 {
-    public class SearchViewModel
-    {
-        public List<activity> activity { get; set; }
-        public List<hotel> hotel { get; set; }
-        public List<restaurant> restaurant { get; set; }
-        public List<spot> spot { get; set; }
-        public IPagedList<spot> show_spot { get; set; }
-        public List<member_profile> member { get; set; }
-        public List<comment> comment { get; set; }
-        public List<collections_detail> collections_detail { get; set; }
-        public IPagedList<result_rating> show_ratings { get; set; }
-        public List<result_rating> result_ratings { get; set; }
-    }
-    public class result_rating 
-    {
-        public string id { get; set; }
-        public string title { get; set; }
-        public string city { get; set; }
-        public string district { get; set; }
-        public string intro { get; set; }
-        public string rating { get; set; }
-        //public int count { get; set; }
-    }
+  public class SearchViewModel
+  {
+    public List<activity> activity { get; set; }
+    public List<hotel> hotel { get; set; }
+    public List<restaurant> restaurant { get; set; }
+    public List<spot> spot { get; set; }
+    public IPagedList<spot> show_spot { get; set; }
+    public List<member_profile> member { get; set; }
+    public List<comment> comment { get; set; }
+    public List<collections_detail> collections_detail { get; set; }
+    public IPagedList<result_rating> show_ratings { get; set; }
+    public List<result_rating> result_ratings { get; set; }
+  }
+  public class result_rating
+  {
+    public string id { get; set; }
+    public string title { get; set; }
+    public string city { get; set; }
+    public string district { get; set; }
+    public string intro { get; set; }
+    public string rating { get; set; }
+  }
+
+  public class NewSearchViewModel 
+  {
+    public string id { get; set; }
+    public string title { get; set; }
+    public string city { get; set; }
+    public string district { get; set; }
+    public string intro { get; set; }
+    public string rating { get; set; }
+    public int commentCount { get; set; }
+    public bool collected { get; set; }
+  }
 
 }
